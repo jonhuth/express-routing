37ed7c5b5b752ec165581f7bccc5f0d99105a966
Jonathan Huth <jonhuth95@gmail.com>
2020-06-17 15:29:12 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const ExpressError = require("./expressError");
+const { mean, median, mode } = require("./helpers");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/mean", function(req, res, next) {
+  let nums = req.query.nums;
+  try {
+    if (!nums) throw new ExpressError("nums are required", 400);
+    nums = nums.split(',').map(num => +num);
+    if (nums.includes(NaN)) throw new ExpressError("input includes NaN", 400);
+
+    let result = mean(nums);
+    return res.json( {response: {
+      operation: "mean",
+      value: result
+    }})
+  } catch (err) {
+    return next(err);
+  }
+    
+})
+
+app.get("/median", function(req, res, next) {
+  try {
+    let nums = req.query.nums;
+    if (!nums) throw new ExpressError("nums are required", 400);
+    nums = nums.split(',').map(num => +num);
+    if (nums.includes(NaN)) throw new ExpressError("input includes NaN", 400);
+
+    let result = median(nums);
+    return res.json( {response: {
+      operation: "median",
+      value: result
+    }})
+  } catch (err) {
+    return next(err);
+  }
+})
+app.get("/mode", function(req, res, next) {
+  try {
+    let nums = req.query.nums;
+    if (!nums) throw new ExpressError("nums are required", 400);
+    nums = nums.split(',').map(num => +num);
+    if (nums.includes(NaN)) throw new ExpressError("input includes NaN", 400);
+    
+    let result = mode(nums);
+
+    return res.json( {response: {
+      operation: "mode",
+      value: result
+    }})
+  } catch(err) {
+    return next(err);
+  }
+})
+
+
+
+app.use(function(err, req, res, next) {
+  // the default status is 500 Internal Server Error
+  let status = err.status || 500;
+  let message = err.message;
+
+  // set the status and alert the user
+  return res.status(status).json({
+    error: { message, status }
+  });
+});
+
+app.listen(3000, function() {
+  console.log("App on port 3000");
+})
---
+++ helpers.js
@@ -0,0 +1,44 @@
+function mean(nums) {
+  let sum = 0;
+  for (let num of nums) {
+    sum += num;
+  }
+  return sum / nums.length;
+}
+
+function median(nums) {
+  if (nums.length % 2) {
+    return nums[Math.floor(nums.length / 2)];
+  }
+  return (nums[nums.length / 2] + nums[(nums.length / 2) - 1]) / 2;
+}
+
+function mode(nums) {
+  let counts = makeCounts(nums);
+  let mode = Number.MIN_VALUE;
+  let highestCount = 0;
+  for (let numKey in counts) {//set mode to key with highest count
+    if (counts[numKey] > highestCount) {
+      mode = numKey;
+      highestCount = counts[numKey];
+    }
+  }
+  return mode;
+}
+
+function makeCounts(nums) {
+  let counts = {};
+  for (let num of nums) {
+    if (num in counts) {
+      counts[num] += 1;
+    }
+    else {
+      counts[num] = 1;
+    }
+  }
+  return counts;
+}
+
+
+
+module.exports = { mean, median, mode };
